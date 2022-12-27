793c7008f8477f90120a1cadc18d2913545c0a86
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-28 00:02:24 +0530

    [Prashant]:Inheritance Program

---
+++ InheritanceProject/InheritanceProject/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceProject
+{
+    public class Class2 : Class1
+    {
+        
+    }
+}
---
+++ InheritanceProject/InheritanceProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace InheritanceProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2 c = new Class2();
+            c.Test1();
+            c.Test2();
+        }
+    }
+}
+
